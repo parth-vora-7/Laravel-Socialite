5b7fcdff173218167f6126dc9256bf130457eacd
Parth vora <parth.vora.777@gmail.com>
2017-08-09 12:40:27 +0530

    DB change

---
+++ README.md
@@ -0,0 +1,21 @@
+Add following into .env file:
+
+APP_URL=YOUR_SITE_URL
+
+github_client_id=ID
+github_client_secret=SECRET
+
+twitter_client_id=ID
+twitter_client_secret=SECRET
+
+bitbucket_client_id=ID
+bitbucket_client_secret=SECRET
+
+linkedin_client_id=ID
+linkedin_client_secret=SECRET
+
+google_client_id=ID
+google_client_secret=SECRET
+
+facebook_client_id=ID
+facebook_client_secret=SECRET
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('social_provider_id');
+            $table->string('social_provider_id')->nullable();
             $table->string('name');
             $table->string('email')->nullable();
             $table->string('password')->nullable();
